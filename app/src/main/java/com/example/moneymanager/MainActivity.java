0050056571343ab2fccb6bacd327a3804bed299c
kubi <pietrusjakub@gmail.com>
2020-05-10 10:37:40 +0200

    Add Firebase configuration

--- app/src/main/java/com/example/moneymanager/MainActivity.java
+++ app/src/main/java/com/example/moneymanager/MainActivity.java
@@ -2,13 +2,73 @@ package com.example.moneymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnLogin;
+    private TextView mForgetPassword;
+    private TextView mSignupHere;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginDetails();
+
+    }
+
+
+    private void loginDetails(){
+
+        mEmail = findViewById(R.id.et_email_login);
+        mPass = findViewById(R.id.et_password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        mForgetPassword = findViewById(R.id.tv_forget_password);
+        mSignupHere = findViewById(R.id.tv_signup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email jest wymagany!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPass.setError("Hasło jest wymagane!");
+                    return;
+                }
+
+            }
+        });
+
+        // Registration activity
+        mSignupHere.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegistrationActivity.class));
+            }
+        });
+
+        // Reset password activity
+        mForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ResetPasswordActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/moneymanager/RegistrationActivity.java
+++ app/src/main/java/com/example/moneymanager/RegistrationActivity.java
@@ -2,13 +2,63 @@ package com.example.moneymanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPass;
+    private Button btnReg;
+    private TextView mSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        registration();
+
     }
+
+    private void registration() {
+
+        mEmail = findViewById(R.id.et_email_reg);
+        mPass = findViewById(R.id.et_password_reg);
+        btnReg = findViewById(R.id.btn_reg);
+        mSignin = findViewById(R.id.tv_signup);
+
+        btnReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String password = mPass.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email jest wymagany!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPass.setError("Hasło jest wymagane!");
+                }
+            }
+        });
+
+
+        mSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+    }
+
+
+
+
 }
